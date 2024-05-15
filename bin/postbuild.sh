894b5354fde70ad33e0bd620efde0ad6b35ee3fe
takagen <takagen@amazon.co.jp>
2024-05-15 16:41:23 +0900

    productionビルド

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -3,11 +3,14 @@
 rm -rf ./.amplify-hosting
 
 mkdir -p ./.amplify-hosting/compute
-mkdir -p ./.amplify-hosting/static
 
 cp -r ./dist/amplify-ssr-angular ./.amplify-hosting/compute/default
 
-# cp -r ./static ./.amplify-hosting
-cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
+mv ./.amplify-hosting/compute/default/browser ./.amplify-hosting/static
+# cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
+cp package.json ./.amplify-hosting/compute/default/package.json
 
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
+
+cd ./.amplify-hosting/compute/default
+npm i --omit=dev
